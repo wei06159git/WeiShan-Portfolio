1c6d6526d5685379c01d5d8df4b66974c0f9b311
Wei-Shan Sun <54604907+wei06159git@users.noreply.github.com>
2019-10-13 21:01:52 -0700

    Add files via upload

---
+++ Sun-FavoriteNumbers-1.py
@@ -0,0 +1,15 @@
+#Read file and print the content
+with open("Sun-favoriteNumbers.txt") as file_ob:
+        lines = file_ob.readlines()
+
+for values in lines:
+    print(values, end=' ')
+
+name = input('Who\'s favorite number do you want to know?\n---> ')
+# Find one's favorite number
+for i in range(len(lines)):
+    if name in lines[i]:
+        dummy = lines[i].split('\t\t\t')
+        number = dummy[1]
+        number = number.rstrip('\n')
+        print(name + '\'s favorite number is: ' + number)
---
+++ Sun-FavoriteNumbers.py
@@ -0,0 +1,14 @@
+# prompt user to enter fav numbers and then write into the file
+dummy = True
+while dummy == True:
+    username = input("\nEnter your name: ")
+    nums = input('What is your favorite number? ')
+
+    with open("Sun-favoriteNumbers.txt", 'a') as file_ob:
+        file_ob.write(username + '\t\t\t' + nums + '\n') 
+
+    response = input('\nDo you want to continue? enter y (yes) or n (no) ')
+    if response == 'y':
+        dummy = True
+    else:
+        dummy = False
