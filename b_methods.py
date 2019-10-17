f93618614b29fcb21efddc5bfe630d7aa23811c0
Wei-Shan Sun <54604907+wei06159git@users.noreply.github.com>
2019-10-16 21:20:04 -0700

    Update b_methods.py

--- b_methods.py
+++ b_methods.py
@@ -1,10 +1,7 @@
+# String Method
+# Coder: Wei-Shan Sun
 #list method
     #A list method can perform a useful operation on a list such as adding or removing elements, sorting, reversing, etc.
-
-#Can you locate list methods in the online Python documentation?
-print('Use this link to python documentation about list method: https://docs.python.org/3/tutorial/datastructures.html')
-
-###Challenge 1###
 
 # Sort short_names in reverse alphabetic order.
 
